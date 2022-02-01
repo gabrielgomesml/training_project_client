643137bcb568d219f50fe1c711948cce9af282eb
gabrielgomesml <xgabrielgomesmelo@gmail.com>
2022-02-01 09:19:55 -0300

    Feature(Login-components): change login component from class to function

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable react/prefer-stateless-function */
-import React from 'react';
+import React, { useState } from 'react';
 import theme from '../../assets/styles/theme';
 import { Button, Input } from '../../components';
 import Logo from '../../assets/icons/cinema.png';
@@ -10,34 +9,35 @@ import {
   LogoImage,
 } from './style';
 
-class Login extends React.Component {
-  render(): React.ReactNode {
-    return (
-      <MainContainer>
-        <ContentContainer>
-          <LogoImage src={Logo} />
-          <InputsContainer>
-            <Input
-              placeholderName="Email"
-              type="text"
-              onChangeAction={() => console.log('oi')}
-            />
-            <Input
-              placeholderName="Senha"
-              type="password"
-              onChangeAction={() => console.log('oi')}
-            />
-          </InputsContainer>
-          <Button
-            width={250}
-            height={45}
-            backgroundColor={theme.colors.mainRed}
-            text="Login"
+const Login: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <MainContainer>
+      <ContentContainer>
+        <LogoImage src={Logo} />
+        <InputsContainer>
+          <Input
+            placeholderName="Email"
+            type="text"
+            onChangeAction={setEmail}
           />
-        </ContentContainer>
-      </MainContainer>
-    );
-  }
-}
+          <Input
+            placeholderName="Senha"
+            type="password"
+            onChangeAction={setPassword}
+          />
+        </InputsContainer>
+        <Button
+          width={250}
+          height={45}
+          backgroundColor={theme.colors.mainRed}
+          text="Login"
+        />
+      </ContentContainer>
+    </MainContainer>
+  );
+};
 
 export default Login;
